6ef6f5bde543c24f7f136e9af8ec931155a1f2df
zyongjun <zhyongjun@windhike.cn>
2016-11-04 23:14:04 +0800

    format week

--- schedule-calendar/src/main/java/com/windhike/calendar/model/CustomDate.java
+++ schedule-calendar/src/main/java/com/windhike/calendar/model/CustomDate.java
@@ -125,6 +125,27 @@ public class CustomDate implements Serializable{
 		}
 		return null;
 	}
+
+	public static String getDisplayFormatWeek(int week){
+		switch (week){
+			case 0:
+				return "星期日";
+			case 1:
+				return "星期一";
+			case 2:
+				return "星期二";
+			case 3:
+				return "星期三";
+			case 4:
+				return "星期四";
+			case 5:
+				return "星期五";
+			case 6:
+				return "星期六";
+
+		}
+		return null;
+	}
 	public void setWeek(int week) {
 		this.week = week;
 	}
